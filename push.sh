57a685659fa48e6b145af709151e76c3afa57626
12buntu <hollarskyle@gmail.com>
2020-02-04 16:46:05 -0500

    this is a commit

--- push.sh
+++ push.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
-echo -e "What is your name?"
+echo -e "What is your name?\n"
 read name
 date=$( date +%m/%d/%y )
 git checkout -B $date-$name
 git add --all
-echo -e "This should push code from your computer to the master (default) branch of the git repository as well as create a new branch for today, for backup\n"
-echo -e "\nCommit message, (type 'skip' if you used Android studio to create the commit" 
+echo -e "This should push code from your computer to the master (default) branch of the git repository as well as create a new branch for today, for backup"
+
+
+echo -e "\nCommit message: (type 'skip' if you used Android studio to create the commit\n" 
 read commitMessage
 case $commitMessage in
         skip) 
         ;;
 
-        *)	git add --all
+        *)	
 		git commit -m "$commitMessage"
 esac
 
 git push -u origin $date-$name
-echo -e "\nMerge with master (yes/no)\n"
+merge
+
+function merge {
+echo -e "Merge with master (yes/no)\n"
 read yesorno
 case $yesorno in
 	yes)
-		git merge ${date}-$name
 		git checkout master
 		git add --all
-		git commit -m "merged from ${date}-$name"
+		git merge $date-$name
+		git commit -m "merged from $date-$name"
 		git push -u origin master
 		;;
 	no)
-
+		
 		;;
 	*)
-		echo "Invalid Response";;
+		echo "Invalid Response"
+		merge
+		;;
 esac
+}
